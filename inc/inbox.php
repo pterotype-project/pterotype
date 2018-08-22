2f699caf44ca3eb395c087ed3f1e7d6f2bc69167
Jeremy Dormitzer <jeremy.dormitzer@gmail.com>
2018-08-22 08:33:21 -0400

    Take some notes on inbox and outbox

--- inc/inbox.php
+++ inc/inbox.php
@@ -1,3 +1,9 @@
 <?php
+/*
+When an Activity is received (i.e. POSTed) to an Actor's inbox, the server must:
 
+  1. Forward the Activity as necessary
+       See (https://www.w3.org/TR/activitypub/#x7-1-2-forwarding-from-inbox).
+  2. Perform the side effects of receiving the Activity
+*/
 ?>
--- inc/outbox.php
+++ inc/outbox.php
@@ -1,3 +1,12 @@
 <?php
+/*
+When an Activity is received (i.e. POSTed) to an Actor's outbox, the server must:
 
+  1. Add the Activity to the Actor's outbox collection in the DB
+  2. Deliver the Activity to the appropriate inboxes based on the received Activity
+       This involves discovering all the inboxes, including nested ones if the target
+       is a collection, deduplicating inboxes, and the POSTing the Activity to each
+       target inbox.
+  3. Perform side effects as necessary
+*/
 ?>
