093c06e7e0a809ad7486fee737c30e11ce24c11a
Jeremy Dormitzer <jeremy.dormitzer@gmail.com>
2018-08-22 08:34:28 -0400

    Add a note

--- inc/outbox.php
+++ inc/outbox.php
@@ -2,6 +2,7 @@
 /*
 When an Activity is received (i.e. POSTed) to an Actor's outbox, the server must:
 
+  0. Make sure the request is authenticated
   1. Add the Activity to the Actor's outbox collection in the DB
   2. Deliver the Activity to the appropriate inboxes based on the received Activity
        This involves discovering all the inboxes, including nested ones if the target
