11038262eb19be5b915ef9abbab68cad429a8af5
Jeremy Dormitzer <jeremy.dormitzer@gmail.com>
2018-10-24 20:32:05 -0400

    Calculate current url correctly

--- includes/server/api.php
+++ includes/server/api.php
@@ -119,9 +119,7 @@ function handle_non_api_requests() {
     global $wp_query;
     $accept = $_SERVER['HTTP_ACCEPT'];
     if ( strpos( $accept, 'application/ld+json' ) !== false ) {
-        $current_url = \trailingslashit(
-            home_url( add_query_arg( $_GET, $wp->request ) )
-        );
+        $current_url = home_url( add_query_arg( $_GET, \trailingslashit( $wp->request ) ) );
         $objects = \pterotype\objects\get_objects_by( 'url', $current_url );
         if ( count( $objects ) > 0 ) {
             $object = $objects[0];
