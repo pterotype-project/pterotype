3fac21cd315b4f93040c52bafe6a07565c13ee1c
Jeremy Dormitzer <jeremy.dormitzer@gmail.com>
2018-11-27 08:58:24 -0500

    Don't call empty on a return value

--- includes/init.php
+++ includes/init.php
@@ -36,7 +36,7 @@ add_action( 'user_register', function( $user_id ) {
 add_action( 'pterotype_init', function() {
     \pterotype\schema\run_migrations();
     \pterotype\actors\initialize_actors();
-    if ( ! empty( ob_get_contents() ) ) {
+    if ( ob_get_contents() ) {
         \error_log( ob_get_contents() );
     }
 } );
